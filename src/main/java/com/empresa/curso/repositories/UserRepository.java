98f0cd382df099326a1c42245dd0075f00582a45
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-04-30 15:20:35 -0300

    Camada de serviço e registro de componentes

--- src/main/java/com/empresa/curso/repositories/UserRepository.java
+++ src/main/java/com/empresa/curso/repositories/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.empresa.curso.entities.User;
 
+// não precisa de anotação de repository, porque já está herdando o Jpa,
+// e o Jpa já é registrado no java spring. se tratando de injeção de dependencia.
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }

451209a81e673c4cea89931d7096c84617ca0f6e
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-05-01 11:52:39 -0300

    Product

---
+++ src/main/java/com/empresa/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.empresa.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.empresa.curso.entities.Product;
+import com.empresa.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
